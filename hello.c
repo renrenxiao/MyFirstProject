8929c1c2aff40c147c684a7870fb236385a13790
RenRenXiao <3070338890@qq.com>
2021-07-28 13:25:27 +0800

    Test "test"Branch

--- hello.c
+++ hello.c
@@ -4,11 +4,18 @@ int add(int a, int b) {
     return a + b;
 }
 
+int mul(int a, int b) {
+    return a * b;
+}
+
 int main() {
     printf("Hello,World!");
     int num1 = 2;
     int num2 = 3;
-    int acc = add(num1,num2);
-    printf("The result is ",acc);
+    int acc_1 = add(num1,num2);
+    printf("The result is ",acc_1);
+
+    int acc_2 = mul(num1,num2);
+    printf("The result is ",acc_2);
     return 0;
 }
