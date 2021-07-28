aab56d769fcf9b1db36671b626560648b83b2f39
RenRenXiao <3070338890@qq.com>
2021-07-28 13:13:19 +0800

    Test Add Funtion

--- hello.c
+++ hello.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
-int main(){
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
     printf("Hello,World!");
+    int num1 = 2;
+    int num2 = 3;
+    int acc = add(num1,num2);
+    printf("The result is ",acc);
     return 0;
 }
